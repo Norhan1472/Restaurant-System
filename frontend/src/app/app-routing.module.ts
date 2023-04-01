cfde8ac67de1b79b7b505626923fa96ca3651bfc
Norhan1472 <nmohamed1472000@gmail.com>
2023-04-01 06:44:47 +0200

    routing in angular

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryItemsComponent } from './components/category-items/category-items.component';
+import { OrderItemsComponent } from './components/order-items/order-items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'allCategories',component:CategoryItemsComponent},
+  {path:'allOrders',component:OrderItemsComponent},
+  {path:'',redirectTo:'/allOrders',pathMatch:'full'},
+  {path:'**',redirectTo:'/allOrders',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
